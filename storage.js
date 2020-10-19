a3f2dac9e88d480af5f8fcceded82028310b4e53
Sławek Rudawski <slawomir.rudawski@gmail.com>
2020-10-19 15:42:18 +0200

    Fixed storage functionality

--- storage.js
+++ storage.js
@@ -1,8 +1,9 @@
-var csvWriter = require('csv-write-stream');
+const csvWriter = require('csv-write-stream');
+const fs = require('fs');
 
 const saveReadingsToCsv = (device, reading01, reading02) => {
   var writer = csvWriter();
-  writer.pipe(fs.createWriteStream('./data/aparmtent_env-local.csv'))
+  writer.pipe(fs.createWriteStream('./data/apartment_env-local.csv'))
   writer.write({
     time: new Date().getTime(),
     device,
