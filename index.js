05cdf413e3342d56e09d70b8fb4ab1b1dd1ac897
Sławek Rudawski <slawomir.rudawski@gmail.com>
2020-10-19 10:30:07 +0200

    OneWire class to handle readings

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 const cat = async path => {
-  new Promise((resolve, reject) => {
-    fs.readFile(path, (err, data) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
         reject(err)
       } else {
@@ -12,8 +12,13 @@ const cat = async path => {
   })
 }
 
-const readOneWire = async () => {
-  console.log(await cat(`/sys/devices/w1_bus_master1/w1_master_slave_count`));
+const oneWire = {
+  bus: {
+    getNumberOfDevices: async () => {
+      console.log(await cat(`/sys/devices/w1_bus_master1/w1_master_slave_count`));
+    }
+  }
 }
 
-readOneWire();
+const numberOfDevices = oneWire.bus.getNumberOfDevices();
+console.log(numberOfDevices);
