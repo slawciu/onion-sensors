1b7316ecf5e19031a416ff11b6cdc6a99060b9d4
Sławek Rudawski <slawomir.rudawski@gmail.com>
2020-10-19 10:33:30 +0200

    Return number instead of console log

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const cat = async path => {
 const oneWire = {
   bus: {
     getNumberOfDevices: async () => {
-      console.log(await cat(`/sys/devices/w1_bus_master1/w1_master_slave_count`));
+      const result = await cat(`/sys/devices/w1_bus_master1/w1_master_slave_count`);
+      return result;
     }
   }
 }
