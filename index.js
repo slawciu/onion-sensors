9c59c0d2aaba052afdf33828e6d714b31a6f5523
Sławek Rudawski <slawomir.rudawski@gmail.com>
2020-10-19 10:43:03 +0200

    Get devices ids from onewire bus

--- index.js
+++ index.js
@@ -17,12 +17,34 @@ const oneWire = {
     getNumberOfDevices: async () => {
       const result = await cat(`/sys/devices/w1_bus_master1/w1_master_slave_count`);
       return Number(result);
+    },
+    getReading: async deviceId => {
+      const result = await cat(`cat /sys/devices/w1_bus_master1/${deviceId}/w1_slave`);
+      return result;
+    },
+    getDevicesIds: async () => {
+      const result = await cat(`cat /sys/devices/w1_bus_master1/w1_master_slaves`);
+      return result;
+    }
+  },
+  sensor: {
+    temperature: {
+      getReading: async deviceId => {
+        const reading = await this.bus.getReading(deviceId);
+        return reading;
+      }
     }
   }
 }
 const main = async () => {
   const numberOfDevices = await oneWire.bus.getNumberOfDevices();
-  console.log(numberOfDevices);
+  console.log(`Detected ${numberOfDevices} device${numberOfDevices > 1 ? 's' : ''}`);
+  const deviceIds = await oneWire.bus.getDevicesIds();
+  console.log(deviceIds);
+  // deviceIds.foreach(deviceId => {
+  //   const reading = await oneWire.sensor.temperature.getReading(deviceId);
+  //   console.log(`${deviceId}: ${reading}`);
+  // })
 }
 
 main();
