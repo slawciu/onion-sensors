daa415a4b604ed8c1c716fb8ce664569bee1cdfe
Sławek Rudawski <slawomir.rudawski@gmail.com>
2020-10-19 10:35:39 +0200

    Return plain number of devices

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const oneWire = {
   bus: {
     getNumberOfDevices: async () => {
       const result = await cat(`/sys/devices/w1_bus_master1/w1_master_slave_count`);
-      return result;
+      return Number(result);
     }
   }
 }
