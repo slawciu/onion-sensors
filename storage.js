6556f39aaaee604b1541dd4f7ebad75904e41b78
Sławek Rudawski <slawomir.rudawski@gmail.com>
2020-10-20 15:29:20 +0200

    Append measurements to csv file

--- storage.js
+++ storage.js
@@ -2,8 +2,8 @@ const csvWriter = require('csv-write-stream');
 const fs = require('fs');
 
 const saveReadingsToCsv = (device, reading01, reading02) => {
-  var writer = csvWriter();
-  writer.pipe(fs.createWriteStream('./data/apartment_env-local.csv', {flags: 'a'}))
+  var writer = csvWriter({sendHeaders: false});
+  writer.pipe(fs.createWriteStream(`./data/${new Date().getTime()}apartment_env-local.csv`, {flags: 'a'}))
   writer.write({
     time: new Date().getTime(),
     device,
