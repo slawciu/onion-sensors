b108057ed3e2d4abef08404178b275a50e7af05c
Sławek Rudawski <slawomir.rudawski@gmail.com>
2020-10-20 16:28:35 +0200

    Save readings in date related files

--- storage.js
+++ storage.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const saveReadingsToCsv = (device, reading01, reading02) => {
   var writer = csvWriter({sendHeaders: false});
-  writer.pipe(fs.createWriteStream(`./data/${new Date().getTime()}apartment_env-local.csv`, {flags: 'a'}))
+  writer.pipe(fs.createWriteStream(`./data/${new Date().toISOString().split('T')[0]}apartment_env-local.csv`, {flags: 'a'}))
   writer.write({
     time: new Date().getTime(),
     device,
